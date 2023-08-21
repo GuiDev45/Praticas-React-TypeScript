c34282bcd33ab168fa502a323ef1cb96d80de7b9
GuiDev45 <bortolettodev@gmail.com>
2023-08-21 20:11:56 -0300

    0400-Projeto Final / Graficos 2 - 15

--- src/0400-Projeto-TSX/Components/GraficoVendas.tsx
+++ src/0400-Projeto-TSX/Components/GraficoVendas.tsx
@@ -10,31 +10,40 @@ import {
 } from "recharts";
 import { IVenda } from "../Context/DataContext";
 
-const dadosGrafico = [
-  {
-    data: "2023-05-03",
-    pago: 30000,
-    processando: 3000,
-    falha: 2000,
-  },
-  {
-    data: "2023-05-04",
-    pago: 340000,
-    processando: 5000,
-    falha: 6000,
-  },
-  {
-    data: "2023-05-05",
-    pago: 250000,
-    processando: 200000,
-    falha: 30000,
-  },
-];
+type VendaDia = {
+  data: string;
+  pago: number;
+  processando: number;
+  falha: number;
+};
+
+function transformData(data: IVenda[]): VendaDia[] {
+  const dias = data.reduce((acc: { [key: string]: VendaDia }, item) => {
+    const dia = item.data.split(" ")[0];
+    if (!acc[dia]) {
+      acc[dia] = {
+        data: dia,
+        pago: 0,
+        falha: 0,
+        processando: 0,
+      };
+    }
+    acc[dia][item.status] += item.preco;
+    return acc;
+  }, {});
+
+  return Object.values(dias).map((dia) => ({
+    ...dia,
+    data: dia.data.substring(5),
+  }));
+}
 
 const GraficoVendas = ({ data }: { data: IVenda[] }) => {
+  const transformedData = transformData(data);
+
   return (
     <ResponsiveContainer width={"99%"} height={400}>
-      <LineChart data={dadosGrafico}>
+      <LineChart data={transformedData}>
         <XAxis dataKey="data" />
         <YAxis />
         <Tooltip />
