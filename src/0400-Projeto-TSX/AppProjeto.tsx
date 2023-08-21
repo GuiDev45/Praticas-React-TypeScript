543153f66429e8cccf2acbbca88948d7b88fbacf
GuilhermeGeplant <guilherme@geplant.com.br>
2023-08-21 11:21:27 -0300

    0400-Projeto Final / Router - 12

--- src/0400-Projeto-TSX/AppProjeto.tsx
+++ src/0400-Projeto-TSX/AppProjeto.tsx
@@ -1,23 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
 import Sidenav from "./Components/Sidenav";
 import { DataContextProvider } from "./Context/DataContext";
 import Resumo from "./Pages/Resumo";
 import Vendas from "./Pages/Vendas";
+import Venda from "./Pages/Venda";
 import "./Style.css";
 
 function AppProjeto() {
   return (
-    // Envolve os componentes com o DataContextProvider para fornecer contexto de dados
-    <DataContextProvider>
-      <div className="container">
-        <Sidenav />
-        <main>
-          <Header /> {/*Formulários*/}
-          <Resumo /> {/*Dados*/}
-          <Vendas />
-        </main>
-      </div>
-    </DataContextProvider>
+    <BrowserRouter>
+      <DataContextProvider>
+        <div className="container">
+          <Sidenav />
+          <main>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Resumo />} />
+              <Route path="/vendas" element={<Vendas />} />
+              <Route path="/vendas/:id" element={<Venda />} />
+            </Routes>
+          </main>
+        </div>
+      </DataContextProvider>
+    </BrowserRouter>
   );
 }
 
